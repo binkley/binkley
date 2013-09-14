ae00fb3da4b04664a3bd6a281a6782e25e8bdd12
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2013-09-14 18:51:49 +0800

    Basic code and sample use.  No javadocs.

--- xnum/src/main/java/hm/binkley/util/Xnum.java
+++ xnum/src/main/java/hm/binkley/util/Xnum.java
@@ -8,7 +8,8 @@ import javax.annotation.Nonnull;
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
  * @todo Needs documentation.
  */
-public abstract class Xnum<X extends Xnum<X>> {
+public abstract class Xnum<X extends Xnum<X>>
+        implements Comparable<X> {
     private final String name;
     private final int ordinal;
 
@@ -24,5 +25,45 @@ public abstract class Xnum<X extends Xnum<X>> {
 
     public final int ordinal() {
         return ordinal;
+    }
+
+    @Override
+    public final int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    public final boolean equals(final Object obj) {
+        return this == obj;
+    }
+
+    @Override
+    protected final Object clone()
+            throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    protected final void finalize() {
+    }
+
+    public final int compareTo(final X o) {
+        final Xnum that = (Xnum) o;
+        if (getClass() != that.getClass() && // optimization
+                getDeclaringClass() != that.getDeclaringClass())
+            throw new ClassCastException();
+        return this.ordinal - that.ordinal;
+    }
+
+    @SuppressWarnings("unchecked")
+    public final Class<X> getDeclaringClass() {
+        final Class clazz = getClass();
+        final Class zuper = clazz.getSuperclass();
+        return (zuper == Xnum.class) ? clazz : zuper;
     }
 }
