f3a685d7ccf2d02bf8edb4ada78bf8feaa8e7822
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2013-09-16 20:33:13 +0800

    Fix compiling.

--- xnum/src/test/java/hm/binkley/util/EgXnum.java
+++ xnum/src/test/java/hm/binkley/util/EgXnum.java
@@ -6,9 +6,6 @@ import com.google.common.collect.ImmutableList;
 import javax.annotation.Nonnull;
 import java.util.List;
 
-import static com.google.common.collect.Iterables.filter;
-import static hm.binkley.util.EgXnum.OfType.ofType;
-
 /**
  * {@code EgXnum} needs documentation.
  *
@@ -31,10 +28,12 @@ public abstract class EgXnum<T>
         return VALUES;
     }
 
+    /*
     @Nonnull
     public static <T> List<EgXnum<T>> valuesOfType(@Nonnull final Class<T> type) {
         return ImmutableList.<EgXnum<T>>copyOf(filter(values(), ofType(type)));
     }
+    */
 
     public abstract T get();
 
