bb80f53acebe4d2a287a8ff468b872f10700951a
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2013-10-14 22:17:02 +0800

    First pass at owner module.
    Guice does not work, that I can tell, with generic providers.  For now
    require each owner config class to have it's own module.

--- guice/src/main/java/hm/binkley/inject/OwnerModule.java
+++ guice/src/main/java/hm/binkley/inject/OwnerModule.java
@@ -1,6 +1,11 @@
 package hm.binkley.inject;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
+
+import javax.annotation.Nonnull;
 
 /**
  * {@code OwnerModule} needs documentation.
@@ -8,10 +13,21 @@ import com.google.inject.AbstractModule;
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
  * @todo Needs documentation.
  */
-public class OwnerModule
+public final class OwnerModule<C extends Config>
         extends AbstractModule {
+    private final Class<C> configType;
+
+    private OwnerModule(final Class<C> configType) {
+        this.configType = configType;
+    }
+
+    public static <C extends Config> Module ownerModule(@Nonnull final Class<C> configType) {
+        return new OwnerModule<>(configType);
+    }
+
     @Override
     protected void configure() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        bind(configType).toInstance(
+                ConfigFactory.create(configType, System.getProperties(), System.getenv()));
     }
 }
--- guice/src/test/java/hm/binkley/inject/OwnerModuleTest.java
+++ guice/src/test/java/hm/binkley/inject/OwnerModuleTest.java
@@ -1,10 +1,15 @@
 package hm.binkley.inject;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import org.aeonbits.owner.Config;
 import org.junit.Test;
 
 import javax.inject.Inject;
+
+import static com.google.inject.Guice.createInjector;
+import static hm.binkley.inject.OwnerModule.ownerModule;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * {@code OwnerModuleTest} tests {@link OwnerModule}.
@@ -14,14 +19,22 @@ import javax.inject.Inject;
 public class OwnerModuleTest {
     @Test
     public void should() {
-        final Injector guice = Guice.createInjector(new OwnerModule());
+        assertThat(createInjector(ownerModule(BobConfig.class)).getInstance(Bob.class).config.foo(),
+                is(equalTo(3)));
     }
 
+    public interface BobConfig
+            extends Config {
+        @DefaultValue("3")
+        int foo();
+    }
 
     public static final class Bob {
-        @Inject
-        public Bob() {
+        private final BobConfig config;
 
+        @Inject
+        public Bob(final BobConfig config) {
+            this.config = config;
         }
     }
 }
