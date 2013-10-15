db5f22c966bfa245d61bae0e88d06ef4548fbb2d
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2013-10-15 10:01:51 +0800

    Include overrides for owner module.
    For example, commandline options might be passed in as overrides; they take
    precedence over environment or system properties.

--- guice/src/main/java/hm/binkley/inject/OwnerModule.java
+++ guice/src/main/java/hm/binkley/inject/OwnerModule.java
@@ -6,6 +6,8 @@ import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
 
 import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@code OwnerModule} is light-weight wiring of an OWNER API config instance into Guice.
@@ -17,9 +19,15 @@ import javax.annotation.Nonnull;
 public final class OwnerModule<C extends Config>
         extends AbstractModule {
     private final Class<C> configType;
+    private final Map<String, String> overrides = new HashMap<>();
 
     private OwnerModule(final Class<C> configType) {
         this.configType = configType;
+    }
+
+    private OwnerModule(final Class<C> configType, final Map<String, String> overrides) {
+        this.configType = configType;
+        this.overrides.putAll(overrides);
     }
 
     /**
@@ -35,9 +43,25 @@ public final class OwnerModule<C extends Config>
         return new OwnerModule<>(configType);
     }
 
+    /**
+     * Creates a new Guice module for the given OWNER API config class with <var>overrides</var>,
+     * for example from commandline options.
+     *
+     * @param configType the config class, never missing
+     * @param overrides more configuration, never missing
+     * @param <C> the config type
+     *
+     * @return the Guice module, never missing
+     */
+    @Nonnull
+    public static <C extends Config> Module ownerModule(@Nonnull final Class<C> configType,
+            @Nonnull final Map<String, String> overrides) {
+        return new OwnerModule<>(configType, overrides);
+    }
+
     @Override
     protected void configure() {
-        bind(configType).toInstance(
-                ConfigFactory.create(configType, System.getProperties(), System.getenv()));
+        bind(configType).toInstance(ConfigFactory
+                .create(configType, overrides, System.getProperties(), System.getenv()));
     }
 }
