42174e19d7617863e43edde011ff7b6d0220fa46
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2013-11-24 14:33:11 +0800

    Begin non-singleton version.

--- logging/src/main/java/hm/binkley/util/logging/osi/OSI.java
+++ logging/src/main/java/hm/binkley/util/logging/osi/OSI.java
@@ -1,3 +1,9 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Please see https://github.com/binkley/binkley/blob/master/LICENSE.md.
+ */
+
 package hm.binkley.util.logging.osi;
 
 import ch.qos.logback.classic.LoggerContext;
@@ -15,20 +21,47 @@ import static org.slf4j.LoggerFactory.getILoggerFactory;
  * @todo Support singleton and non-singleton modes
  * @todo Implement {@link java.lang.AutoCloseable} when non-singleton
  */
-public final class OSI {
+public final class OSI
+        implements AutoCloseable {
+    private final LoggerContext context = new LoggerContext();
+
+    /**
+     * @todo Support non-singleton version - bits of logback are package private for this
+     */
     public static void enableLogging() {
         setProperty("logback.configurationFile", "osi-logback.xml");
     }
 
     public static void reconfigureLogging()
             throws JoranException {
-        final LoggerContext loggerContext = (LoggerContext) getILoggerFactory();
+        reconfigureLogging0((LoggerContext) getILoggerFactory());
+    }
+
+    public static void stopLoggingThreads() {
+        stopLoggingThreads0((LoggerContext) getILoggerFactory());
+    }
+
+    private static void stopLoggingThreads0(final LoggerContext loggerContext) {
+        loggerContext.stop();
+    }
+
+    /**
+     * @todo Do not use {@link ContextInitializer#autoConfig()} directly - assumes sysprops
+     */
+    private static void reconfigureLogging0(final LoggerContext loggerContext)
+            throws JoranException {
         loggerContext.reset();
         loggerContext.getStatusManager().clear();
         new ContextInitializer(loggerContext).autoConfig();
     }
 
-    public static void stopLoggingThreads() {
-        ((LoggerContext) getILoggerFactory()).stop();
+    public void reconfigure()
+            throws JoranException {
+        reconfigureLogging0(context);
+    }
+
+    @Override
+    public void close() {
+        stopLoggingThreads0(context);
     }
 }
