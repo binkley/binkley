48f0596738f3e7320381a2a586069ec90da40a30
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2014-04-08 06:54:10 +0800

    Merge.

--- spring/src/main/java/hm/binkley/dao/SimpleDAO.java
+++ spring/src/main/java/hm/binkley/dao/SimpleDAO.java
@@ -1,5 +1,6 @@
 package hm.binkley.dao;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -7,6 +8,8 @@ import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * A very simple DAO wrapper for using Spring transactions and JDBC template, designed for lambda
@@ -33,6 +36,16 @@ public class SimpleDAO {
     }
 
     /**
+     * Access to the underlying transaction manager.
+     *
+     * @return the transaction manager, never missing
+     */
+    @Nonnull
+    public DataSourceTransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+    /**
      * Runs the given <var>dao</var> within Spring transaction.
      *
      * @param dao the dao callback, never missing
@@ -45,10 +58,36 @@ public class SimpleDAO {
     }
 
     /**
+     * Runs the given <var>dao</var> within Spring transaction, requiring 0 or 1 results.
+     *
+     * @param dao the dao callback, never missing
+     * @param wrongSize the exception factory for more than 0 or 1 results, never missing
+     * @param <T> the return type of the callback
+     *
+     * @return an optional of the return
+     *
+     * @throws DataAccessException if the results are the wrong size
+     */
+    public final <T> Optional<T> daoMaybe(@Nonnull final Dao<List<T>> dao,
+            @Nonnull final WrongSize wrongSize) {
+        final List<T> result = dao(dao);
+        final int size = result.size();
+        switch (size) {
+        case 0:
+            return Optional.empty();
+        case 1:
+            return Optional.of(result.get(0));
+        default:
+            throw wrongSize.fail(size);
+        }
+    }
+
+    /**
      * The functional interface for {@link #dao(Dao)}.
      *
      * @param <T> the callback return type
      */
+    @FunctionalInterface
     public interface Dao<T> {
         /**
          * Manages the JDBC callback, wrapping it in a Spring transaction.  The JDBC template passed
@@ -74,5 +113,10 @@ public class SimpleDAO {
          * @return the callback result
          */
         T with(@Nonnull final JdbcTemplate jdbcTemplate, @Nonnull final TransactionStatus status);
+    }
+
+    @FunctionalInterface
+    public interface WrongSize {
+        DataAccessException fail(final int size);
     }
 }
