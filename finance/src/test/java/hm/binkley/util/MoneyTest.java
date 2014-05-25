b6fd8428af60597ee17dced1f28e291fb5a1cea9
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2014-05-26 06:39:12 +0800

    Abstract testing.

--- finance/src/test/java/hm/binkley/util/MoneyTest.java
+++ finance/src/test/java/hm/binkley/util/MoneyTest.java
@@ -23,7 +23,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Currency;
 import java.util.List;
+import java.util.function.Function;
 
+import static java.util.function.Function.identity;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -33,16 +35,37 @@ public class MoneyTest {
     @Parameters(name = "{index}: {0}: {1}")
     public static Collection<Object[]> parameters()
             throws IOException {
-        final List<Object[]> parameters = new ArrayList<>();
-        final Ini ini = new Ini(MoneyTest.class.getResource("MoneyTest.ini"));
-        for (final Section section : ini.values()) {
-            final String currency = section.get("currency");
-            final String amount = section.get("amount");
-            parameters.add(array(section.getName(), section.get("value"),
-                    null == currency ? null : Currency.getInstance(currency),
-                    null == amount ? null : new BigDecimal(amount)));
+        return parameters(new Ini(MoneyTest.class.getResource("MoneyTest.ini")),
+                Key.of("value", identity()), Key.of("currency", Currency::getInstance),
+                Key.of("amount", BigDecimal::new));
+    }
+
+    public static final class Key {
+        public final String name;
+        public final Function<String, ?> get;
+
+        public static Key of(final String name, final Function<String, ?> get) {
+            return new Key(name, get);
         }
 
+        private Key(final String name, final Function<String, ?> get) {
+            this.name = name;
+            this.get = get;
+        }
+    }
+
+    public static List<Object[]> parameters(final Ini ini, final Key... keys) {
+        final List<Object[]> parameters = new ArrayList<>();
+        for (final Section section : ini.values()) {
+            final Object[] array = new Object[1 + keys.length];
+            array[0] = section.getName();
+            for (int i = 0; i < keys.length; i++) {
+                final Key key = keys[i];
+                final String value = section.fetch(key.name);
+                array[1 + i] = null == value ? null : key.get.apply(value);
+            }
+            parameters.add(array);
+        }
         return parameters;
     }
 
