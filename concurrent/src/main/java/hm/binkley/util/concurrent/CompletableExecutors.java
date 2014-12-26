c93d447eb8319f69d3318660f4231ca4a5c1b9b0
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2014-12-26 11:18:49 +0800

    Drop wrapped/unwrapped distinction.

--- concurrent/src/main/java/hm/binkley/util/concurrent/CompletableExecutors.java
+++ concurrent/src/main/java/hm/binkley/util/concurrent/CompletableExecutors.java
@@ -40,20 +40,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Supplier;
 
 import static hm.binkley.util.Mixin.newMixin;
 import static java.util.concurrent.Executors.callable;
 
 /**
  * {@code CompleteableExecutors} are executors returning {@link
- * CompletableFuture} rather than plain {@link Future}.  They also support
- * {@code Closeable.close()} to shutdown the thread pool.
+ * CompletableFuture} rather than plain {@link Future}.
  * <p>
  * Additionally, JDK completable futures wrap {@code InterruptedException} with
- * {@code ExecutionException}.  Use {@link #completable(ExecutorService)} to
- * preserve this behavior, {@link #unwrappedCompletable(ExecutorService)} to
- * have the {@code InterruptedException} thrown on {@code get()}.
+ * {@code ExecutionException}.  These are unwrapped to provide expected behavior
+ * with {@link Future#get()} and friends.
+ * <p>
+ * Lastly, these executors expose {@code Closeable.close()} to shutdown the
+ * thread pool in support of the <em>try-with-resources</em> idiom.
  *
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
  * @todo Think through completable for scheduled
@@ -71,23 +71,7 @@ public final class CompletableExecutors {
     public static CompletableExecutorService completable(
             @Nonnull final ExecutorService threads) {
         return newMixin(CompletableExecutorService.class,
-                new Overrides(threads, CompletableFuture::new), threads);
-    }
-
-    /**
-     * Mixes the given <var>threads</var> (execution service) with overrides to
-     * provide a completable exection service.
-     *
-     * @param threads the execution service, never missin
-     *
-     * @return the completable execution service, never missing
-     */
-    @Nonnull
-    public static CompletableExecutorService unwrappedCompletable(
-            @Nonnull final ExecutorService threads) {
-        return newMixin(CompletableExecutorService.class,
-                new Overrides(threads, UnwrappedCompletableFuture::new),
-                threads);
+                new Overrides(threads), threads);
     }
 
     /**
@@ -133,19 +117,15 @@ public final class CompletableExecutors {
      */
     public static final class Overrides {
         private final ExecutorService threads;
-        private final Supplier<CompletableFuture<?>> ctor;
 
-        private Overrides(final ExecutorService threads,
-                final Supplier<CompletableFuture<?>> ctor) {
+        private Overrides(final ExecutorService threads) {
             this.threads = threads;
-            this.ctor = ctor;
         }
 
         @Nonnull
         public <T> CompletableFuture<T> submit(
                 @Nonnull final Callable<T> task) {
-            @SuppressWarnings("unchecked") final CompletableFuture<T> cf
-                    = (CompletableFuture<T>) ctor.get();
+            final CompletableFuture<T> cf = new UnwrappedCompletableFuture<>();
             threads.submit(() -> {
                 try {
                     cf.complete(task.call());
