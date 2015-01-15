fcd540accd71c2e6e8f54498250a293ac8c6a669
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-01-16 07:33:12 +0800

    Compiles (no thanks to Spring).

--- yaml/src/test/java/hm/binkley/annotation/processing/YamlGenerateProcessorTest.java
+++ yaml/src/test/java/hm/binkley/annotation/processing/YamlGenerateProcessorTest.java
@@ -1,12 +1,24 @@
 package hm.binkley.annotation.processing;
 
 import hm.binkley.annotation.YamlGenerate;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * {@code YamlGenerateProcessorTest} tests {@link YamlGenerateProcessor}.
  *
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley</a>
  */
+public final class YamlGenerateProcessorTest {
+    @Test
+    public void shouldGenerate() {
+        assertThat(new fooby.Howard().baz(), is(equalTo("stringy!")));
+    }
+}
+
 @YamlGenerate(template = "/foo/test.ftl",
         inputs = {"/foo/*.yml", "/bar/3.yml"}, namespace = "fooby")
-public interface YamlGenerateProcessorTest {}
+interface YamlGenerateTestClasses {}
