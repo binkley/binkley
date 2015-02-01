c2e21782a77b25a7c63a65c739815b61258feeef
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-02-01 18:42:12 +0800

    Fix NonRandom, but it works only for ints.

--- testing/src/main/java/hm/binkley/util/NonRandom.java
+++ testing/src/main/java/hm/binkley/util/NonRandom.java
@@ -4,7 +4,8 @@ import java.util.NoSuchElementException;
 import java.util.Random;
 
 /**
- * {@code NonRandom} provides deterministic random numbers for testing.
+ * {@code NonRandom} provides deterministic random numbers for testing
+ * specifically for {@link #nextInt()} and {@link #nextInt(int)}.
  *
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
  */
@@ -23,19 +24,25 @@ public class NonRandom
         this.nexts = nexts;
     }
 
+    @Override
+    public int nextInt() {
+        return nextInt(Integer.MAX_VALUE);
+    }
+
     /**
      * Provides the next available element from {@link #nexts}.
      *
      * @return the next {@code int}
      *
      * @throws NoSuchElementException if {@link #nexts} is exhausted.
-     * @todo More clever way to handle bits == 32?
      */
     @Override
-    protected int next(final int bits) {
+    public int nextInt(final int bound) {
         if (i == nexts.length)
             throw new NoSuchElementException("Exhausted random numbers");
         final int next = nexts[i++];
-        return 32 == bits ? next : next & Integer.MAX_VALUE >> 31 - bits;
+        if (next > bound)
+            throw new IllegalArgumentException("Bound less than nexts");
+        return next;
     }
 }
