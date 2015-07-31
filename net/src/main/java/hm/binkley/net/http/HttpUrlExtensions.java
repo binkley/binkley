bb7b64215fb01ca8406d7438d9352bf0b985c158
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-07-31 11:45:49 -0500

    Some docs.

--- net/src/main/java/hm/binkley/net/http/HttpUrlExtensions.java
+++ net/src/main/java/hm/binkley/net/http/HttpUrlExtensions.java
@@ -3,6 +3,8 @@ package hm.binkley.net.http;
 import com.squareup.okhttp.HttpUrl;
 import lombok.EqualsAndHashCode;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -22,13 +24,21 @@ import static java.util.stream.Collectors.toMap;
 import static java.util.stream.IntStream.range;
 
 /**
- * {@code HttpUrlExtensions} <strong>needs documentation</strong>.
+ * {@code HttpUrlExtensions} is a Lombok <a href="https://projectlombok.org/features/experimental/ExtensionMethod.html">method
+ * extension</a> for {@link HttpUrl} from <a href="http://square.github.io/okhttp/">OkHttp</a>.
  *
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
  * @todo Needs documentation
  */
 public final class HttpUrlExtensions {
-    public static List<PathSegment> pathSegmentMap(final HttpUrl url) {
+    /**
+     * Creates a list of path segments for the given <var>url</var>, each
+     * segment being a mapping of a named path component to any path
+     * parameters (matrix parameters).
+     */
+    @Nonnull
+    public static List<PathSegment> pathSegmentMap(
+            @Nonnull final HttpUrl url) {
         final List<String> segments = url.pathSegments();
         final List<PathSegment> pathSegments = new ArrayList<>(
                 segments.size());
@@ -37,11 +47,16 @@ public final class HttpUrlExtensions {
                 collect(toCollection(() -> pathSegments));
     }
 
+    /**
+     * Creates a map of query parameters for the given <var>url</var>,
+     * iterable in the same order the parameters appear in the URL.
+     */
+    @Nonnull
     public static Map<String, List<String>> queryParameterMap(
-            final HttpUrl url) {
+            @Nonnull final HttpUrl url) {
         final Set<String> names = url.queryParameterNames();
-        final LinkedHashMap<String, List<String>> queries
-                = new LinkedHashMap<>(names.size());
+        final Map<String, List<String>> queries = new LinkedHashMap<>(
+                names.size());
         return unmodifiableMap(names.stream().
                 collect(toMap(identity(), url::queryParameterValues,
                         (a, b) -> {
@@ -80,23 +95,33 @@ public final class HttpUrlExtensions {
 
             final Map<String, List<String>> params = new LinkedHashMap<>();
             lumps.subList(1, lumps.size()).stream().
-                    map(l -> asList(equals.split(l, 2))).
+                    map(l -> equals.split(l, 2)).
                     forEach(p -> params.
-                            computeIfAbsent(p.get(0), k -> new ArrayList<>()).
-                            add(2 == p.size() ? p.get(1) : null));
+                            computeIfAbsent(p[0], k -> new ArrayList<>()).
+                            add(2 == p.length ? p[1] : null));
             params.entrySet().stream().
                     forEach(e -> e.setValue(unmodifiableList(e.getValue())));
             this.params = unmodifiableMap(params);
         }
 
+        /** The position of this path segment in the URL, 0-based. */
+        @Nonnegative
         public int position() {
             return position;
         }
 
+        /** The "name" or path component of this segment in the URL. */
+        @Nonnull
         public String name() {
             return name;
         }
 
+        /**
+         * The unmodifiable map of path parameters for this path segment,
+         * iterable in the same order as the path segments appear in the URL.
+         * Similarly, path parameters iterate in their order of appearance.
+         */
+        @Nonnull
         public Map<String, List<String>> params() {
             return params;
         }
