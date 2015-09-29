df72ff8bfe8e8f517a5203f6fe6947b4f7d69ee3
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-09-29 15:09:07 -0500

    Cleaner test using DSL.

--- magic-bus/src/test/java/hm/binkley/MagicBusTest.java
+++ magic-bus/src/test/java/hm/binkley/MagicBusTest.java
@@ -3,6 +3,7 @@ package hm.binkley;
 import hm.binkley.MagicBus.FailedMessage;
 import hm.binkley.MagicBus.Mailbox;
 import hm.binkley.MagicBus.UnsubscribedMessage;
+import lombok.RequiredArgsConstructor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,7 +14,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 import static hm.binkley.MagicBus.discard;
+import static lombok.AccessLevel.PRIVATE;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -29,8 +33,8 @@ public final class MagicBusTest {
 
     @Before
     public void setUpFixture() {
-        returned = new ArrayList<>();
-        failed = new ArrayList<>();
+        returned = new ArrayList<>(1);
+        failed = new ArrayList<>(1);
         bus = new MagicBus(returned::add, failed::add);
     }
 
@@ -54,7 +58,7 @@ public final class MagicBusTest {
 
     @SuppressWarnings("ConstantConditions")
     @Test(expected = NullPointerException.class)
-    public void shouldRejectMissingHandlerInSubscribe() {
+    public void shouldRejectMissingMailboxInSubscribe() {
         bus.subscribe(RightType.class, null);
     }
 
@@ -66,7 +70,7 @@ public final class MagicBusTest {
 
     @SuppressWarnings("ConstantConditions")
     @Test(expected = NullPointerException.class)
-    public void shouldRejectMissingHandlerInUnsubscribe() {
+    public void shouldRejectMissingMailboxInUnsubscribe() {
         bus.unsubscribe(RightType.class, null);
     }
 
@@ -78,48 +82,65 @@ public final class MagicBusTest {
 
     @Test
     public void shouldReceiveCorrectType() {
-        final List<RightType> mailbox = new ArrayList<>();
-        bus.subscribe(RightType.class, mailbox::add);
+        final List<RightType> messages = new ArrayList<>(1);
+        bus.subscribe(RightType.class, messages::add);
 
         bus.publish(new RightType());
 
-        assertThat(mailbox.isEmpty(), is(false));
+        assertOn(messages).
+                delivered(1).
+                returned(0).
+                failed(0);
     }
 
     @Test
     public void shouldNotReceiveWrongType() {
-        final List<LeftType> mailbox = new ArrayList<>();
-        bus.subscribe(LeftType.class, mailbox::add);
+        final List<LeftType> messages = new ArrayList<>(1);
+        bus.subscribe(LeftType.class, messages::add);
 
         bus.publish(new RightType());
 
-        assertThat(mailbox.isEmpty(), is(true));
+        assertOn(messages).
+                delivered(0).
+                returned(1).
+                failed(0);
     }
 
     @Test
     public void shouldReceiveSubtypes() {
-        final List<BaseType> mailbox = new ArrayList<>();
-        bus.subscribe(BaseType.class, mailbox::add);
+        final List<BaseType> messages = new ArrayList<>(1);
+        bus.subscribe(BaseType.class, messages::add);
 
         bus.publish(new RightType());
 
-        assertThat(mailbox.isEmpty(), is(false));
+        assertOn(messages).
+                delivered(1).
+                returned(0).
+                failed(0);
     }
 
     @Test
     public void shouldSaveDeadLetters() {
+        final List<BaseType> messages = new ArrayList<>(0);
         bus.publish(new LeftType());
 
-        assertThat(returned.isEmpty(), is(false));
+        assertOn(messages).
+                delivered(0).
+                returned(1).
+                failed(0);
     }
 
     @Test
     public void shouldSaveFailedPosts() {
+        final List<BaseType> messages = new ArrayList<>(0);
         bus.subscribe(LeftType.class, failWith(Exception::new));
 
         bus.publish(new LeftType());
 
-        assertThat(failed.isEmpty(), is(false));
+        assertOn(messages).
+                delivered(0).
+                returned(0).
+                failed(1);
     }
 
     @Test(expected = RuntimeException.class)
@@ -148,6 +169,10 @@ public final class MagicBusTest {
         assertThat(second.get(), is(equalTo(1)));
         assertThat(third.get(), is(equalTo(2)));
         assertThat(fourth.get(), is(equalTo(3)));
+
+        assertOn(null).
+                returned(0).
+                failed(0);
     }
 
     @Test
@@ -170,6 +195,10 @@ public final class MagicBusTest {
         assertThat(base.get(), is(equalTo(1)));
         assertThat(right.get(), is(equalTo(2)));
         assertThat(farRight.get(), is(equalTo(3)));
+
+        assertOn(null).
+                returned(0).
+                failed(0);
     }
 
     @Test
@@ -177,6 +206,9 @@ public final class MagicBusTest {
         bus = new MagicBus(discard(), discard());
 
         bus.publish(new RightType());
+
+        assertThat(returned, is(empty()));
+        assertThat(failed, is(empty()));
     }
 
     @Test
@@ -187,26 +219,73 @@ public final class MagicBusTest {
         });
 
         bus.publish(new RightType());
+
+        assertThat(returned, is(empty()));
+        assertThat(failed, is(empty()));
     }
 
     @Test
-    public void shouldUnsubscribe() {
-        final Mailbox<RightType> mailbox = m -> {
-            throw new Exception();
-        };
+    public void shouldUnsubscribeOnlyMailbox() {
+        final List<RightType> messages = new ArrayList<>(0);
+        final Mailbox<RightType> mailbox = messages::add;
         bus.subscribe(RightType.class, mailbox);
         bus.unsubscribe(RightType.class, mailbox);
 
         bus.publish(new RightType());
 
-        assertThat(returned.isEmpty(), is(false));
+        assertOn(messages).
+                delivered(0).
+                returned(1).
+                failed(0);
+    }
+
+    @Test
+    public void shouldUnsubscribeRightMailbox() {
+        final List<RightType> messagesA = new ArrayList<>(1);
+        final Mailbox<RightType> mailboxB = m -> {
+            throw new Exception();
+        };
+        bus.subscribe(RightType.class, messagesA::add);
+        bus.subscribe(RightType.class, mailboxB);
+        bus.unsubscribe(RightType.class, mailboxB);
+
+        bus.publish(new RightType());
+
+        assertOn(messagesA).
+                delivered(1).
+                returned(0).
+                failed(0);
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void shouldComplainWhenUnsubscribingNonSubscribedMailbox() {
+    public void shouldComplainWhenUnsubscribingBadMailbox() {
         final Mailbox<RightType> mailbox = m -> {};
 
         bus.unsubscribe(RightType.class, mailbox);
+    }
+
+    private AssertDelivery assertOn(final List messages) {
+        return new AssertDelivery(messages);
+    }
+
+    @RequiredArgsConstructor(access = PRIVATE)
+    private final class AssertDelivery {
+        private final List<?> messages;
+
+        private AssertDelivery delivered(final int delivered) {
+            assertThat(messages, hasSize(delivered));
+            return this;
+        }
+
+        private AssertDelivery returned(final int returned) {
+            assertThat(MagicBusTest.this.returned, hasSize(returned));
+            return this;
+        }
+
+        private AssertDelivery failed(final int failed) {
+            assertThat(MagicBusTest.this.failed, hasSize(failed));
+            return this;
+        }
     }
 
     private static <T> Mailbox<T> record(final AtomicInteger order,
