9a858b0f03dd4b0bce637f553c6fa0d13340bb13
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-10-25 07:59:31 -0500

    Add Reactor version of magic bus.

---
+++ magic-bus/src/main/java/hm/binkley/ReactorMagicBus.java
@@ -0,0 +1,93 @@
+package hm.binkley;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import reactor.bus.Event;
+import reactor.bus.EventBus;
+import reactor.bus.registry.Registration;
+import reactor.bus.spec.EventBusSpec;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
+import static reactor.bus.Event.wrap;
+import static reactor.bus.selector.Selectors.type;
+
+/**
+ * {@code ReactorMagicBus} is an implementation of {@link MagicBus} based on
+ * <a href="http://projectreactor.io/">Reactor</a>.
+ *
+ * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
+ */
+@RequiredArgsConstructor
+public final class ReactorMagicBus
+        implements MagicBus {
+    private final EventBus bus = new EventBusSpec().
+            synchronousDispatcher().
+            uncaughtErrorHandler(ReactorMagicBus::rethrowUnchecked).
+            get();
+    private final Map<Subscription<?>, Registration<Object, reactor.fn.Consumer<? extends Event<?>>>>
+            subscriptions = new ConcurrentHashMap<>();
+
+    private final Consumer<? super ReturnedMessage> returned;
+    private final Consumer<? super FailedMessage> failed;
+
+    public <T> void subscribe(@Nonnull final Class<T> type,
+            @Nonnull final Mailbox<? super T> mailbox) {
+        subscriptions.put(Subscription
+                        .of(requireNonNull(type), requireNonNull(mailbox)),
+                bus.on(type(type), event -> receive(mailbox, event)));
+    }
+
+    public <T> void unsubscribe(@Nonnull final Class<T> type,
+            @Nonnull final Mailbox<? super T> mailbox) {
+        ofNullable(subscriptions.remove(Subscription
+                .of(requireNonNull(type), requireNonNull(mailbox)))).
+                orElseThrow(NoSuchElementException::new).
+                cancel();
+    }
+
+    public void post(@Nonnull final Object message) {
+        final Class<?> type = message.getClass();
+        if (bus.respondsToKey(type)) {
+            bus.notify(type, wrap(message));
+            return;
+        }
+
+        returned.accept(new ReturnedMessage(this, message));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> void receive(final Mailbox<? super T> mailbox,
+            final Event<?> event) {
+        final T message = (T) event.getData();
+        try {
+            mailbox.receive(message);
+        } catch (final RuntimeException e) {
+            throw e;
+        } catch (final Exception e) {
+            failed.accept(new FailedMessage(this, mailbox, message, e));
+        }
+    }
+
+    private static void rethrowUnchecked(final Throwable t) {
+        if (t instanceof RuntimeException)
+            throw (RuntimeException) t;
+        else if (t instanceof Error)
+            throw (Error) t;
+        else
+            throw new RuntimeException("BUG: Did not handle " + t);
+    }
+
+    @EqualsAndHashCode
+    @RequiredArgsConstructor(staticName = "of")
+    private static final class Subscription<T> {
+        private final Class<T> type;
+        private final Mailbox<? super T> mailbox;
+    }
+}
---
+++ magic-bus/src/test/java/hm/binkley/ReactorMagicBusTest.java
@@ -0,0 +1,294 @@
+package hm.binkley;
+
+import hm.binkley.MagicBus.Mailbox;
+import org.junit.Test;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.stream.IntStream.range;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * {@code ReactorBusTest} tests {@link ReactorMagicBus}.
+ *
+ * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
+ */
+public final class ReactorMagicBusTest
+        extends MagicBusTestBase<ReactorMagicBus> {
+    public ReactorMagicBusTest() {
+        super(ReactorMagicBus::new);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingReturnedHandlerInConstructor() {
+        new SimpleMagicBus(null, failed::add);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingFailedHandlerInConstructor() {
+        new SimpleMagicBus(returned::add, null);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingMessageTypeInSubscribe() {
+        bus.subscribe(null, message -> {
+        });
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingMailboxInSubscribe() {
+        bus.subscribe(RightType.class, null);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingMessageTypeInUnsubscribe() {
+        bus.unsubscribe(null, message -> {
+        });
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingMailboxInUnsubscribe() {
+        bus.unsubscribe(RightType.class, null);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldRejectMissingMessageInPublish() {
+        bus.post(null);
+    }
+
+    @Test
+    public void shouldReceiveCorrectType() {
+        final List<RightType> messages = new ArrayList<>(1);
+        bus.subscribe(RightType.class, messages::add);
+
+        final RightType message = new RightType();
+        bus.post(message);
+
+        assertOn(messages).
+                delivered(message).
+                noneReturned().
+                noneFailed();
+    }
+
+    @Test
+    public void shouldNotReceiveWrongType() {
+        final List<LeftType> messages = new ArrayList<>(1);
+        bus.subscribe(LeftType.class, messages::add);
+
+        final RightType message = new RightType();
+        bus.post(message);
+
+        assertOn(messages).
+                noneDelivered().
+                returned(with(message)).
+                noneFailed();
+    }
+
+    @Test
+    public void shouldReceiveSubtypes() {
+        final List<BaseType> messages = new ArrayList<>(1);
+        bus.subscribe(BaseType.class, messages::add);
+
+        final RightType message = new RightType();
+        bus.post(message);
+
+        assertOn(messages).
+                delivered(message).
+                noneReturned().
+                noneFailed();
+    }
+
+    @Test
+    public void shouldSaveDeadLetters() {
+        final LeftType message = new LeftType();
+        bus.post(message);
+
+        assertOn(noMailbox()).
+                noneDelivered().
+                returned(with(message)).
+                noneFailed();
+    }
+
+    @Test
+    public void shouldSaveFailedPosts() {
+        final Exception failure = new Exception();
+        final Mailbox<LeftType> mailbox = failWith(() -> failure);
+        bus.subscribe(LeftType.class, mailbox);
+
+        final LeftType message = new LeftType();
+        bus.post(message);
+
+        assertOn(noMailbox()).
+                noneDelivered().
+                noneReturned().
+                failed(with(mailbox, message, failure));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowFailedPostsForUnchecked() {
+        bus.subscribe(LeftType.class, failWith(RuntimeException::new));
+
+        bus.post(new LeftType());
+    }
+
+    @Test
+    public void shouldReceiveEarlierSubscribersFirst() {
+        final AtomicInteger delivery = new AtomicInteger();
+        final AtomicInteger second = new AtomicInteger();
+        final AtomicInteger first = new AtomicInteger();
+        final AtomicInteger fourth = new AtomicInteger();
+        final AtomicInteger third = new AtomicInteger();
+
+        bus.subscribe(RightType.class, record(delivery, first));
+        bus.subscribe(RightType.class, record(delivery, second));
+        bus.subscribe(RightType.class, record(delivery, third));
+        bus.subscribe(RightType.class, record(delivery, fourth));
+
+        bus.post(new RightType());
+
+        assertThat(first.get(), is(equalTo(0)));
+        assertThat(second.get(), is(equalTo(1)));
+        assertThat(third.get(), is(equalTo(2)));
+        assertThat(fourth.get(), is(equalTo(3)));
+
+        assertOn(noMailbox()).
+                noneReturned().
+                noneFailed();
+    }
+
+    @Test
+    public void shouldReceiveOnParentTypeFirst() {
+        final AtomicInteger delivery = new AtomicInteger();
+        final AtomicInteger farRight = new AtomicInteger();
+        final AtomicInteger right = new AtomicInteger();
+        final AtomicInteger base = new AtomicInteger();
+        final AtomicInteger object = new AtomicInteger();
+
+        // Register in "random" order to avoid anomalies of implementation
+        bus.subscribe(RightType.class, record(delivery, right));
+        bus.subscribe(FarRightType.class, record(delivery, farRight));
+        bus.subscribe(Object.class, record(delivery, object));
+        bus.subscribe(BaseType.class, record(delivery, base));
+
+        bus.post(new FarRightType());
+
+        assertThat(object.get(), is(equalTo(0)));
+        assertThat(base.get(), is(equalTo(1)));
+        assertThat(right.get(), is(equalTo(2)));
+        assertThat(farRight.get(), is(equalTo(3)));
+
+        assertOn(noMailbox()).
+                noneReturned().
+                noneFailed();
+    }
+
+    @Test
+    public void shouldUnsubscribeOnlyMailbox() {
+        final List<LeftType> messages = new ArrayList<>(0);
+        final Mailbox<LeftType> mailbox = messages::add;
+        bus.subscribe(LeftType.class, mailbox);
+        bus.unsubscribe(LeftType.class, mailbox);
+
+        final LeftType message = new LeftType();
+        bus.post(message);
+
+        assertOn(messages).
+                returned(with(message)).
+                noneFailed();
+    }
+
+    @Test
+    public void shouldUnsubscribeRightMailbox() {
+        final List<RightType> messagesA = new ArrayList<>(1);
+        final Mailbox<RightType> mailboxB = failWith(Exception::new);
+        bus.subscribe(RightType.class, messagesA::add);
+        bus.subscribe(RightType.class, mailboxB);
+        bus.unsubscribe(RightType.class, mailboxB);
+
+        final RightType message = new RightType();
+        bus.post(message);
+
+        assertOn(messagesA).
+                delivered(message).
+                noneReturned().
+                noneFailed();
+    }
+
+    @Test(timeout = 2000L)
+    public void shouldUnsubscribeThreadSafely()
+            throws InterruptedException {
+        final int actors = 100;
+        final CountDownLatch latch = new CountDownLatch(actors);
+
+        range(0, actors).parallel().
+                forEach(actor -> {
+                    // Use "new" to guarantee a unique instance
+                    final Mailbox<RightType> mailbox = new Discard();
+                    bus.subscribe(RightType.class, mailbox);
+                    bus.unsubscribe(RightType.class, mailbox);
+                    latch.countDown();
+                });
+
+        latch.await(1L, SECONDS);
+
+        final RightType message = new RightType();
+        bus.post(message);
+
+        assertOn(noMailbox()).
+                returned(with(message)).
+                noneFailed();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldComplainWhenUnsubscribingBadMailbox() {
+        final Mailbox<RightType> mailbox = m -> {
+        };
+
+        bus.unsubscribe(RightType.class, mailbox);
+    }
+
+    private static <T> Mailbox<T> record(final AtomicInteger order,
+            final AtomicInteger record) {
+        return m -> record.set(order.getAndIncrement());
+    }
+
+    private static <T, E extends Exception> Mailbox<T> failWith(
+            final Supplier<E> ctor) {
+        return message -> {
+            throw ctor.get();
+        };
+    }
+
+    private static class Discard
+            implements Mailbox<RightType> {
+        @Override
+        public void receive(@Nonnull final RightType message) {}
+    }
+
+    private abstract static class BaseType {}
+
+    private static final class LeftType
+            extends BaseType {}
+
+    private static class RightType
+            extends BaseType {}
+
+    private static final class FarRightType
+            extends RightType {}
+}
